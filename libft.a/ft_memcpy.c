d54de462341bbad1831cea3d7ac8decd0155b946
adhernan <113341886+adhernan-42@users.noreply.github.com>
2022-11-01 15:56:23 +0100

    Add files via upload

--- libft.a/ft_memcpy.c
+++ libft.a/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: adhernan <adhernan@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 16:32:48 by adhernan          #+#    #+#             */
-/*   Updated: 2022/10/25 16:37:45 by adhernan         ###   ########.fr       */
+/*   Updated: 2022/11/01 15:40:18 by adhernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 
 	pdst = (char *) dst;
 	psrc = (const char *) src;
-	if (pdst == NULL && psrc == NULL)
-		return (NULL);
 	while (n--)
-		*(pdst++) = *(psrc++);
+		pdst[n] = psrc[n];
 	return (dst);
 }
